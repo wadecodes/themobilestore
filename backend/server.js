5230c6a2384b4f83c5660ce3e770eff096863538
wadecodes <wadecodes@gmail.com>
2020-10-15 18:41:10 +0530

    created Error Handlers

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 
 import connectDB from './config/db.js';
+import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
 
 import productRoutes from './routes/productRoutes.js';
 
@@ -11,11 +12,15 @@ connectDB();
 
 const app = express();
 
-app.use('/api/products', productRoutes);
-
 app.get('/', (req, res) => {
   res.send('API is running..');
 });
+
+app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
