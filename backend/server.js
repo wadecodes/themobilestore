a3691497a7db0dc3e9d422b8e2180304b39b655f
wadecodes <wadecodes@gmail.com>
2020-10-15 16:28:56 +0530

    create backend server and inital products and product route and fetching the products data in the front end using route

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const products = require('./data/products');
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is running..');
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
+  res.json(product);
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
